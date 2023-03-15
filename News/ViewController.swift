ef07ad6334b86de10e9cf634262cbe1a7a74b7ac
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-03-15 20:03:44 +0100

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Kresimir Ivanjko on 15.03.2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
